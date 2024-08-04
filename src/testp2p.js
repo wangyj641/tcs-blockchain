6b76831aa0b6e53a55d44a33d4caf65aca2273f5
Yongjun <wangyj641@gmail.com>
2024-08-04 14:23:45 +0800

    Update comments

---
+++ src/testp2p.js
@@ -0,0 +1,25 @@
+const dgram = require('dgram')
+const udp = dgram.createSocket('udp4')
+
+//udp recv message
+udp.on('message', (data, remote) => {
+  console.log('accept message' + data.toString());
+  console.log(remote);
+})
+udp.on('listening', function () {
+  const address = udp.address()
+  console.log('udp server is listening' + address.address + ":" + address.port);
+})
+udp.bind(8002)
+
+function send(message, port, host) {
+  console.log('send message', message, port, host);
+  udp.send(Buffer.from(message), port, host)
+}
+
+const port = progress.argv[2]
+const host = progress.argv[3]
+if (port && host) {
+  send('你好', port, host)
+}
+
